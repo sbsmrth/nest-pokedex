706bc3272f9756c4610d3d10c5fc5250b4376a0e
sbsmrth <andresf.mrh@gmail.com>
2025-06-23 15:56:04 -0500

    feat(user): add user module with controller, service, DTOs, and entity; integrate into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname, '..', 'public'),
     }),
     PokemonModule,
+    UserModule,
   ],
 })
 export class AppModule {}
