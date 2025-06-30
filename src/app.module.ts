f4817f40fa44bbf915b697667b51f5c2fd5ae573
sbsmrth <andresf.mrh@gmail.com>
2025-06-30 16:46:21 -0500

    fix(config): update env file handling and MongoDB config
    - Update .gitignore to track .env.development and .env.production
    
    - Adjust docker-compose to use MONGO_DB_NAME
    
    - Swap start and start:prod scripts in package.json
    
    - Configure Mongoose connection to use dbName from env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,9 @@ import { JoiValidationSchema } from './config/joi.schema';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
+    MongooseModule.forRoot(process.env.MONGO_URI!, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
